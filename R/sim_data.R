99e634f2907a6cc70ad512e9be8c3e5fa6e73632
denisagniel <dagniel@rand.org>
2020-07-09 16:46:13 -0500

    Expand possible k-l expansions.

--- R/sim_data.R
+++ R/sim_data.R
@@ -1,18 +1,24 @@
-sim_data <- function(n, m, sigma2, delta) {
+sim_data <- function(n, m, sigma2, delta, K = 2, gamma = 2) {
   s <- seq(0,10,length.out = m)
   meanFunct <- function(s) s + 10*exp(-(s-5)^2)
-  eigFunct1 <- function(s) +cos(2*s*pi/10) / sqrt(5)
-  eigFunct2 <- function(s) -sin(2*s*pi/10) / sqrt(5)
-  # eigFunct3 <- function(s) +cos(2*s*pi/20) / sqrt(5)
-  # eigFunct4 <- function(s) -sin(2*s*pi/20) / sqrt(5)
 
-  Ksi <- matrix(rnorm(n*2), ncol=2);
+  efnfn <- function(K) {
+    function(s) {
+      (K %% 2)*cos(2*s*pi/10/ceiling(K/2))/ sqrt(5) +
+        ((K+1) %% 2)*(-sin(2*s*pi/10/ceiling(K/2)))/ sqrt(5)
+    }
+  }
+
+  efns <- purrr::map(1:K, efnfn)
+
+  efn_mat <- dplyr::bind_cols(purrr::map(efns, ~.(s)))
+
+  Ksi <- matrix(rnorm(n*K), ncol=K);
   Ksi <- apply(Ksi, 2, scale)
-  Ksi <- Ksi %*% diag(c(5,2))
+  Ksi <- Ksi %*% diag(1/(1:K)^gamma)*5
 
   # Create Y_true
-  yTrue <- Ksi %*% t(matrix(c(eigFunct1(s),eigFunct2(s)#,eigFunct3(s),eigFunct4(s)
-                              ), ncol=2)) + t(matrix(rep(meanFunct(s),n), nrow=m)) + rnorm(n*m, sd = sigma2) %>% matrix(n, m)
+  yTrue <- Ksi %*% t(efn_mat) + t(matrix(rep(meanFunct(s),n), nrow=m)) + rnorm(n*m, sd = sigma2) %>% matrix(n, m)
 
   ds <- tibble(
     id = rep(1:n, each=m),
