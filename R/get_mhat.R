8ab4524fdeee95ed813256b61ace1f336f5d61fe
denisagniel <dagniel@rand.org>
2020-07-10 11:14:02 -0500

    Fix gam for small n.

--- R/get_mhat.R
+++ R/get_mhat.R
@@ -18,18 +18,27 @@ get_mhat <- function(id, trt, time, y, pre_data, post_data, linear = FALSE) {
     arrange(!!idn) %>%
     pull(!!yn)
 
-  if (linear) {
-    fgam_fit <- fgam(post_y0 ~ lf(pre_y0))
-  } else {
-    fgam_fit <- fgam(post_y0 ~ af(pre_y0))
-  }
+  n <- length(post_y0)
+  lk <- min(c(n-1, floor(n/2), 20))
+  gk <- min(floor(sqrt(c(n-1, n/2, 40))))
 
-  yhat_ds <- pre_y %>%
-    select(!!idn, !!trtn) %>%
-    mutate(yhat_0 = predict(fgam_fit,
-                            newdata= list(pre_y0 =
-                                            pre_y %>%
-                                            select(-!!idn, -!!trtn) %>%
-                                                      as.matrix)))
+  if (linear) {
+    fgam_fit <- try(fgam(post_y0 ~ lf(pre_y0)), silent = TRUE)
+  } else {
+    fgam_fit <- try(fgam(post_y0 ~ af(pre_y0, k = gk)), silent = TRUE)
+  }
+  if (inherits(fgam_fit, 'try-error')) {
+    yhat_ds <- pre_y %>%
+      select(!!idn, !!trtn) %>%
+      mutate(yhat_0 = NA)
+  } else {
+    yhat_ds <- pre_y %>%
+      select(!!idn, !!trtn) %>%
+      mutate(yhat_0 = predict(fgam_fit,
+                              newdata= list(pre_y0 =
+                                              pre_y %>%
+                                              select(-!!idn, -!!trtn) %>%
+                                              as.matrix)))
+  }
   yhat_ds
 }
